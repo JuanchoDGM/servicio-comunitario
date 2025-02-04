5ede51f7325eeec71d701a4b0b25db18630ea70c
Juan David <96352469+JuanchoDGM@users.noreply.github.com>
2025-02-04 13:33:42 -0400

    Actualizar reporte.php

--- reporte.php
+++ reporte.php
@@ -14,7 +14,7 @@ class PDF extends FPDF
         // Movernos a la derecha
         $this->Cell(80);
         // Título
-        $this->Cell(30, 10, 'Reporte de Estudiantes Inscritos', 0, 1, 'C');
+        $this->Cell(30, 10, 'Reporte de Estudiantes Inscritos — 2024-2025', 0, 1, 'C');
         // Salto de línea
         $this->Ln(20);
     }
